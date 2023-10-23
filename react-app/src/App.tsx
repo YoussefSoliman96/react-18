cdd7fb4da3c3dae1b77f2841bc93a179ff412809
YoussefSoliman96 <youssef@DESKTOP-757K3CN>
2023-10-23 21:33:53 +0300

    Add Form

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -6,23 +6,12 @@ import { useState } from "react";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex expedita
-        illum corrupti placeat eos soluta eum consequatur numquam, hic illo cum
-        quaerat laudantium ducimus? Exercitationem, quia nemo vero enim
-        excepturi reiciendis amet at quo, suscipit id, aut est necessitatibus
-        quae eius nisi? Ratione, error ad repudiandae numquam blanditiis maxime
-        odio hic mollitia eum a, commodi expedita inventore ipsa vero aliquam
-        reprehenderit saepe ipsum iste recusandae. Illo pariatur saepe rem
-        similique a aspernatur fugiat, cumque sit iusto repellendus impedit
-        tempora minus voluptatum iure reiciendis dolorum, animi tempore
-        blanditiis, hic fuga vitae ut dolor quos consequuntur! Blanditiis
-        laboriosam nostrum tenetur consequuntur aspernatur!
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,88 @@
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(3, { message: "Name must be at least 3 character(s)" }),
+  age: z.number({ invalid_type_error: "Age must be a number" }).min(18),
+});
+
+const Form = () => {
+  //   interface FormData {
+  //     name: string;
+  //     age: number;
+  //   }
+
+  type FormData = z.infer<typeof schema>;
+
+  // Method 3: [React Form hook]
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          id="name"
+          {...register("name")}
+          type="text"
+          className="form-control"
+        />
+
+        {errors.name && <p className="text-danger">{errors.name.message}</p>}
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          id="age"
+          {...register("age", { valueAsNumber: true })}
+          type="number"
+          className="form-control"
+        />
+        {errors.age && <p className="text-danger">{errors.age.message}</p>}
+      </div>
+      <button disabled={!isValid} className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
+
+// Method 1: [Ref hook]
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = { name: "", age: 0 };
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     if (nameRef.current !== null) {
+//       console.log(`Name is ${nameRef.current.value}`);
+//       person.name = nameRef.current.value;
+//     }
+//     if (ageRef.current !== null) {
+//       console.log(`Age is ${ageRef.current.value}`);
+//       person.age = parseInt(ageRef.current.value);
+//     }
+//     console.log(person);
+
+// Method 2: [State hook]
+//   const [person, setPerson] = useState({
+//     name: "",
+//     age: "",
+//   });
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     console.log(person);
+//   };
